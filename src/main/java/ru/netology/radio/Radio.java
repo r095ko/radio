18e0339ab04f90b376172ba12b9e4f158f03fb6b
r095ko <r095ko@gmail.com>
2022-04-15 14:50:24 +0300

    add next and prev

--- src/main/java/ru/netology/radio/Radio.java
+++ src/main/java/ru/netology/radio/Radio.java
@@ -8,14 +8,33 @@ public class Radio {
         return currentStation;
     }
 
-    public static void setCurrentStation(int newCurrentStation) {
+   static void setCurrentStation(int currentStation) {
         if (currentStation < 0) {
             return;
         }
         if (currentStation > 9) {
             return;
         }
-        currentStation = newCurrentStation;
+        Radio.currentStation = currentStation;
     }
 
+   static int setNextStation(int nextStation) {
+        nextStation = Radio.currentStation;
+        if (currentStation == 9) {
+            nextStation = 0;
+        }
+        return Radio.currentStation = nextStation + 1;
+    }
+
+    static int setPrevStation(int prevStation) {
+            prevStation = Radio.currentStation;
+            if (currentStation == 0) {
+                prevStation = 9;
+            }
+            return Radio.currentStation = prevStation - 1;
+
+        }
+
 }
+
+
--- src/test/java/ru/netology/radio/RadioTest.java
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -8,11 +8,33 @@ import static ru.netology.radio.Radio.*;
 public class RadioTest {
 
     @Test
-    public void test() {
+    public void testCurrent() {
         Radio currentStation = new Radio();
         Radio.setCurrentStation(10);
 
         int expected = 0;
+        int actual = getCurrentStation();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testNext() {
+        Radio currentStation = new Radio();
+        Radio.setNextStation(5);
+
+        int expected = 6;
+        int actual = getCurrentStation();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testPrev() {
+        Radio currentStation = new Radio();
+        Radio.setNextStation(9);
+
+        int expected = 8;
         int actual = getCurrentStation();
 
         assertEquals(expected, actual);
